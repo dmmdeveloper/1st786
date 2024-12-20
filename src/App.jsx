b912afcbbc457f64b2031221e7a084ad43822076
dmmdeveloper <zongdostm@gmail.com>
2024-12-20 00:07:39 -0800

    Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import './App.css'
 function App(params) {
 
   return(<>
-  
-  
-  <h1>Dost Muhammad Malhoo <br /> React @Vite  App</h1>
-  <h2>In the Front Of Rashid **</h2>
+<h1>Todo App</h1>
   
   
   </>)
